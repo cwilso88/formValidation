160bb85dd8d053c7912533fedebeda3dfbd0f2fc
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-19 10:18:45 -0400

    added event listeners for each form label

---
+++ app.js
@@ -0,0 +1,33 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+
+document.getElementById('zip').addEventListener('blur', validateZip);
+
+document.getElementById('email').addEventListener('blur', validateEmail);
+
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+
+
+function validateName() {
+    const name = document.getElementById('name');
+    const re = /^[a-zA-Z]{2,10}$/;
+
+    
+    
+}
+
+function validateZip() {
+
+    
+}
+
+function validateEmail() {
+
+    
+}
+
+function validatePhone() {
+
+    
+}
