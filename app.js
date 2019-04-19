ef97d211dee7eee3fd3ea1c47695458f2acb2c3d
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-19 10:22:37 -0400

    completed validateName function

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ function validateName() {
     const name = document.getElementById('name');
     const re = /^[a-zA-Z]{2,10}$/;
 
-    
+    if(!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.remove('is-invalid');
+    }
     
 }
 
