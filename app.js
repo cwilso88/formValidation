595502796cf4b48bc7710fb2067de6efd18c7e21
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-19 10:59:10 -0400

    completed validatePhone function

--- app.js
+++ app.js
@@ -45,13 +45,13 @@ function validateEmail() {
 }
 
 function validatePhone() {
-    const zip = document.getElementById('zip');
-    const re = /^[0-9]{5}(-[0-9]{4})?$/;
+    const phone = document.getElementById('phone');
+    const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
 
-    if(!re.test(zip.value)) {
-        zip.classList.add('is-invalid');
+    if(!re.test(phone.value)) {
+        phone.classList.add('is-invalid');
     } else {
-        zip.classList.remove('is-invalid');
+        phone.classList.remove('is-invalid');
     }  
     
 }
