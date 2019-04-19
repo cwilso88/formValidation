ea055ba78a67c8998a8d8b8ad85439e85e2e6dd6
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-19 10:44:09 -0400

    completed validateZip function

--- app.js
+++ app.js
@@ -17,17 +17,23 @@ function validateName() {
         name.classList.add('is-invalid');
     } else {
         name.classList.remove('is-invalid');
+    } 
+}
+
+function validateZip() {
+    const zip = document.getElementById('zip');
+    const re = /^[0-9]{5}(-[0-9]{4})?$/;
+
+    if(!re.test(zip.value)) {
+        zip.classList.add('is-invalid');
+    } else {
+        zip.classList.remove('is-invalid');
     }
     
 }
 
-function validateZip() {
-
-    
-}
-
 function validateEmail() {
-
+    
     
 }
 
